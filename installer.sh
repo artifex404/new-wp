96fc69e4203594253527fd078e34a67f4641f418
Igor Buyanov <igor.buyanov@monamiagency.com>
2015-12-04 10:47:13 +0200

    Fix installer typo

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sS https://raw.githubusercontent.com/artifex404/new-wp/master/zip-git.sh -o ~/new-wp.sh
-chmod u+x ~/zip-git.sh
+curl -sS https://raw.githubusercontent.com/artifex404/new-wp/master/new-wp.sh -o ~/new-wp.sh
+chmod u+x ~/new-wp.sh
 echo "alias new-wp='~/new-wp.sh'" >> ~/.bash_profile
 source ~/.bash_profile
 echo -e '\033[1m\033[32mNew-wp installed successfully!\033[0m'
